24237d0ed2c872fb9cdc4d18aa6cac3cd6a31ac4
AyselHeydarova <aysel.esedova1994@gmail.com>
2025-06-16 21:05:04 +0400

    lesson 4

--- src/test/kotlin/com/motycka/edu/lesson04/TemperatureConverterTest.kt
+++ src/test/kotlin/com/motycka/edu/lesson04/TemperatureConverterTest.kt
@@ -6,6 +6,48 @@ import io.kotest.matchers.shouldBe
 // By convention, the name of the test class should be the name of the class under test + "Test"
 class TemperatureConverterTest : StringSpec({
 
-    // TODO Implement tests for the TemperatureConverter class
+    "convert Fahrenheit to Celsius where fahrenheit is 32" {
+        // given
+        val fahrenheit = 32.0
+
+        // when
+        val celsius = TemperatureConverter.toCelsius(fahrenheit)
+
+        // then
+        celsius shouldBe 0.0
+    }
+
+    "convert Fahrenheit to Celsius where fahrenheit is 212" {
+        // given
+        val fahrenheit = 212.0
+
+        // when
+        val celsius = TemperatureConverter.toCelsius(fahrenheit)
+
+        // then
+        celsius shouldBe 100.0
+    }
+
+    "convert Fahrenheit to Celsius where fahrenheit is 98.6" {
+        // given
+        val fahrenheit = 98.6
+
+        // when
+        val celsius = TemperatureConverter.toCelsius(fahrenheit)
+
+        // then
+        celsius shouldBe 37.0
+    }
+
+    "convert Fahrenheit to Celsius where fahrenheit is negative" {
+        // given
+        val fahrenheit = -148.0
+
+        // when
+        val celsius = TemperatureConverter.toCelsius(fahrenheit)
+
+        // then
+        celsius shouldBe -100.0
+    }
 
 })
